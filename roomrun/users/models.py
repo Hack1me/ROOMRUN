f5130cbd243820639609f4f2300dc41d86894fa9
Hack1me <sahitndaheu@gmail.com>
2026-08-22 23:18:28 +0100

    Setting User model

--- roomrun/users/models.py
+++ roomrun/users/models.py
@@ -5,6 +5,7 @@ from typing import ClassVar
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
