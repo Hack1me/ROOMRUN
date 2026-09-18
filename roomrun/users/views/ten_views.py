c6ae4a578ed5635eec4d49a807c72567df23f342
Hack1me <sahitndaheu@gmail.com>
2026-09-18 12:08:27 +0100

    closing with landlord sidebar problem

--- roomrun/users/views/ten_views.py
+++ roomrun/users/views/ten_views.py
@@ -1,5 +1,7 @@
+from django.db.models import Prefetch
 from django.views.generic import ListView
 from properties.mixins import LandlordRequiredMixin
+from rentals.models import RentalContract
 from users.models import Tenant
 from users.services import TenantService
 
@@ -15,12 +17,33 @@ class TenantListView(LandlordRequiredMixin, ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return TenantService.get_list(
+        queryset = TenantService.get_list(
             landlord=self.get_landlord(),
             search=self.request.GET.get("q", ""),
             user_status=self.request.GET.get("status", ""),
             verified=self._get_verified_filter(),
         )
+        return queryset.prefetch_related(
+            Prefetch(
+                "rental_contracts",
+                queryset=RentalContract.objects.select_related(
+                    "unit__building__property_ref",
+                ).order_by("-created_at"),
+                to_attr="listed_contracts",
+            ),
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "active_nav": "tenants",
+                "search": self.request.GET.get("q", ""),
+                "status_filter": self.request.GET.get("status", ""),
+                "verified_filter": self.request.GET.get("verified", ""),
+            },
+        )
+        return context
 
     def _get_verified_filter(self):
         """Parse the `verified` query param into True / False / None."""
